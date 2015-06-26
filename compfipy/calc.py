212b98d6ecc72ae2f003844229ba6c519e20c3bb
tmthydvnprt <tim@tmthydvnprt.com>
2015-06-26 06:39:26 -0700

    add functions

--- compfipy/calc.py
+++ compfipy/calc.py
@@ -25,3 +25,24 @@ def effective_return(r=0.07, f=2.0):
 def annual_return(r=0.07, f=1.0):
     """calculate annual return from semiannual return"""
     return np.power(1 + r, f) - 1
+
+def inflation_adjusted(r=0.07, i=0.03):
+    """inflation adjusted returns"""
+    return (1 + r) / (1 + i) - 1
+
+def gain(xi=100.0, xf=110.0):
+    """calculate gain from intial to final value"""
+    return (xf - xi) / xi
+
+def amortization(p=1000.0, r=0.05, n=10.0, f=1.0):
+    """calculate periodic payments needed to pay off p principle at r rate over n periods every f frequency"""
+    return p * (r / f) / (1 - np.power(1 + r / f, -f*n))
+
+def cagr(xi=100.0, xf=110.0, n=1.0):
+    """compund annual growth rate"""
+    return np.power(xf / xi, 1 / n) - 1
+
+def credit_payment(b=1000.0, p=100.0, apr=0.18):
+    """length of payments of b balance with p payment at apr apr"""
+    i = apr / 30.0
+    return (-1 / 30) * np.log(1 + (b / p)*(1 - np.power(1 + i, 30))) / np.log(1 + i)
