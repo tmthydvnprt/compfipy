4eca1ed00b3d2147e103ed6790d55d0618799668
tmthydvnprt <tim@tmthydvnprt.com>
2015-09-23 07:05:19 -0700

    add drawdown

--- compfipy/strategy.py
+++ compfipy/strategy.py
@@ -20,8 +20,17 @@ class Strategy(object):
         self.short_open = {symbol:False for symbol in portfolio.assets.keys()}
         self.display_data = []
 
-        record_headers = ['buy price','buy shares','buy fees','sell price','sell shares','sell fees','gain','profit','loss','return','win/loose']
-        self.record = {symbol:pd.DataFrame(columns=record_headers) for symbol in portfolio.assets.keys()}
+        recordings = [
+            'buy price', 'buy shares', 'buy fees', 'buy date',
+            'sell price', 'sell shares', 'sell fees', 'sell date',
+            'gain', 'profit', 'loss', 'return', 'win/loose',
+            'min', 'min date', 'max', 'max date',
+            'drawdown', 'drawdown time'
+        ]
+        self.record = {symbol:pd.DataFrame(columns=recordings) for symbol in portfolio.assets.keys()}
+        self.max = {symbol:[0, None] for symbol in portfolio.assets.keys()}
+        self.min = {symbol:[999999999999999, None] for symbol in portfolio.assets.keys()}
+        self.drawdown = {symbol:[999999999999999, None] for symbol in portfolio.assets.keys()}
 
     def on_date(self, date, i):
         """called for each date of portfolio data, implements trading logic"""
@@ -32,18 +41,31 @@ class Strategy(object):
         self.long_open[symbol] = True
         self.portfolio.trade(symbol, date, shares, self.commission_min, self.commission)
 
+        # set min, max & drawdown
+        self.max[symbol] = [self.portfolio.assets[symbol].c[date], date]
+        self.min[symbol] = [self.portfolio.assets[symbol].c[date], date]
+        self.drawdown[symbol] = [self.portfolio.assets[symbol].c[date], date]
+
         record = {
             'buy price' : [self.portfolio.assets[symbol].c[date]],
             'buy shares' : [shares],
             'buy fees' : [max(self.commission_min, abs(self.commission * shares))],
+            'buy date' : [date],
             'sell price' : [None],
             'sell shares' : [None],
             'sell fees' : [None],
+            'sell date' : [None],
             'gain' : [None],
             'profit' : [None],
             'loss' : [None],
             'return' : [None],
-            'win/loose' : [None]
+            'win/loose' : [None],
+            'min':[None],
+            'max':[None],
+            'min date':[None],
+            'max date':[None],
+            'drawdown':[None],
+            'drawdown time':[None]
         }
 
         self.record[symbol] = self.record[symbol].append(pd.DataFrame(record), ignore_index=True)
@@ -58,6 +80,7 @@ class Strategy(object):
         self.record[symbol].loc[i, 'sell price'] = self.portfolio.assets[symbol].c[date]
         self.record[symbol].loc[i, 'sell shares'] = shares
         self.record[symbol].loc[i, 'sell fees'] =  max(self.commission_min, abs(self.commission * shares))
+        self.record[symbol].loc[i, 'sell date'] = date
 
         initial = self.record[symbol].loc[i, 'buy price'] * self.record[symbol].loc[i, 'buy shares']
         final = self.record[symbol].loc[i, 'sell price'] * self.record[symbol].loc[i, 'sell shares']
@@ -76,6 +99,15 @@ class Strategy(object):
             self.record[symbol].loc[i, 'win/loose'] = 'l'
         else:
             self.record[symbol].loc[i, 'win/loose'] = '-'
+
+        self.record[symbol].loc[i, 'min'] = self.min[symbol][0]
+        self.record[symbol].loc[i, 'max'] = self.max[symbol][0]
+
+        self.record[symbol].loc[i, 'min date'] = self.min[symbol][1]
+        self.record[symbol].loc[i, 'max date'] = self.max[symbol][1]
+
+        self.record[symbol].loc[i, 'drawdown'] = self.max[symbol][0] - self.drawdown[symbol][0]
+        self.record[symbol].loc[i, 'drawdown time'] = self.drawdown[symbol][1] - self.max[symbol][1]
 
     def enter_short(self):
         self.short_open[symbol] = True
@@ -107,7 +139,7 @@ class Strategy(object):
         performance = self.calc_performance()
 
         assets = [' '] + performance.keys()
-        perf = [performance.values()[0].keys()] + [[fmtn(p) for p in asset_performance.values()] for asset_performance in performance.values()]
+        perf = [performance.values()[0].keys()] + [[p for p in asset_performance.values()] for asset_performance in performance.values()]
         perf = [list(x) for x in zip(*perf)]
 
         print tabulate.tabulate(perf, headers=assets, floatfmt=".2f")
@@ -133,6 +165,10 @@ class Strategy(object):
                 washes = len(self.record[symbol].groupby('win/loose').groups['-'])
             except:
                 washes = 0
+            max_drawdown = self.record[symbol]['drawdown'].max()
+            average_drawdown = self.record[symbol]['drawdown'].mean()
+            max_drawdown_time = self.record[symbol]['drawdown time'].max()
+            average_drawdown_time = self.record[symbol]['drawdown time'].mean()
 
             performance[symbol] = {
                 'trades': trades,
@@ -144,12 +180,14 @@ class Strategy(object):
                 'net_profit': profit - loss,
                 'profit_factor': profit / loss,
                 'percent_profitable': wins / trades,
-                'average_trade_net_profit' : (profit - loss) / trades
+                'average_trade_net_profit' : (profit - loss) / trades,
+                'max_drawdown' : max_drawdown,
+                'average_drawdown' : average_drawdown,
+                'max_drawdown_time' : max_drawdown_time,
+                'average_drawdown_time' : average_drawdown_time
             }
 
         return performance
-
-
 
 ################################################################################################################################
 
@@ -164,6 +202,20 @@ class Sma_Xover(Strategy):
         for symbol in self.portfolio.assets.keys():
 
             if len(self.portfolio.assets[symbol].c[:date]) > 200:
+
+                price = self.portfolio.assets[symbol].c[date]
+
+                # record min, max & drawdown
+                if price > self.max[symbol][0]:
+                    self.max[symbol] = [price, date]
+                    # reset drawdown
+                    self.drawdown[symbol] = [price, date]
+
+                if price < self.min[symbol][0]:
+                    self.min[symbol] = [price, date]
+
+                if price < self.drawdown[symbol][0]:
+                    self.drawdown[symbol] = [price, date]
 
                 # calculate moving averages
                 sma50 = sma(self.portfolio.assets[symbol].c[:date], 50).iloc[-2:]
