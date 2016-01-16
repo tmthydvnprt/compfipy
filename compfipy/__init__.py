001bd68766381924a36dd11a38049dd16ed2ac8c
tmthydvnprt <tim@tmthydvnprt.com>
2016-01-16 06:41:56 -0800

    update init

--- compfipy/__init__.py
+++ compfipy/__init__.py
@@ -1,4 +1,16 @@
 """
 compfipy
+Computational Finance in Python
 """
-pass
+
+__all__ = ['asset', 'portfolio', 'market', 'models', 'calculator']
+__version__ = '0.1.0'
+__date__ = '2015-06-14 05:15:58 -0700'
+__author__ = 'tmthydvnprt'
+__status__ = 'development'
+__website__ = 'https://github.com/tmthydvnprt/compfipy'
+__email__ = 'tmthydvnprt@users.noreply.github.com'
+__maintainer__ = 'tmthydvnprt'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015, tmthydvnprt'
+__credits__ = ''
